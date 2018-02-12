04a0f99480a9d62793d164fb543d1c09e4b287d3
nick <pocknick08@gmail.com>
2018-02-12 16:39:15 +0800

    *new

--- TESTER/src/TestTypeConversion.java
+++ TESTER/src/TestTypeConversion.java
@@ -1,19 +1,22 @@
-/**
- * 
- */
-
-/**
- * @author User
- *
- */
 public class TestTypeConversion {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
+		Double salaryDbl = new Double(args[0]);
+		if (salaryDbl.isNaN()) {
+			System.out.println("Salaryis not valid:" + salaryDbl);
+		} 
+		
+		else {
+			
+		double salary = salaryDbl.doubleValue();
+	
+		String salaryStr = String.valueOf(salary);
+		
+		System.out.println(salaryStr);
+		
 	}
 
-}
+}}
